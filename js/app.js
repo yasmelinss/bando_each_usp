308117efd43ce56ab2a706efd6e77589ab555b2f
Yasmin Messias <yasminmessias@MacBook-Pro-de-Yasmin.local>
2024-09-01 00:26:46 -0300

    AAA

---
+++ js/app.js
@@ -0,0 +1,63 @@
+// Debounce do Lodash
+debounce = function(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+
+(function(){
+	var $target = $('.im'),
+			animationClass = 'anime-start',
+			offset = $(window).height() * 3/4;
+
+	function animeScroll() {
+		var documentTop = $(document).scrollTop();
+
+		$target.each(function(){
+			var itemTop = $(this).offset().top;
+			if (documentTop > itemTop - offset) {
+				$(this).addClass(animationClass);
+			} else {
+				$(this).removeClass(animationClass);
+			}
+		});
+	}
+
+	animeScroll();
+
+	$(document).scroll(debounce(function(){
+		animeScroll();
+	}, 200));
+})();
+
+//login
+var usuarios = [
+    {"login": "bcf", "senha": "bcf"},
+    {"login": "mamae", "senha": "abacaxi"},
+    {"login": "papai", "senha": "melancia"},
+];
+
+function Login() {
+    var usuario = document.getElementsByName('username')[0].value.toLowerCase();
+    var senha = document.getElementsByName('password')[0].value;
+
+    for (var u in usuarios) {
+        var us = usuarios[u];
+        if (us.login === usuario && us.senha === senha) {
+            window.location = "index.html";
+            return true;
+        }
+    }
+    alert("Dados incorretos, tente novamente.");
+    return false;
+}
